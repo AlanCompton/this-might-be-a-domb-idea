ea20b4282a19780079468c8b96d9741faaa94ca6
AlanCompton <alan.compton@codeimmersives.com>
2022-10-24 15:00:29 -0500

    Completed

---
+++ main.js
@@ -0,0 +1,24 @@
+let paragraph = document.querySelector('#paragraph');
+paragraph.style.color = 'lightblue';
+
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+let header = document.querySelector('#header');
+header.style.fontSize = '48px';
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '.5';
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = 'I say, "Hi!';
+
+let myImg = document.querySelector('#myImg');
+myImg.style.height = '300px';
+
+let yourImg = document.querySelector('img');
+yourImg.src = 'https://passionpassport-1.s3.amazonaws.com/wp-content/uploads/2018/01/09185958/mt-fuji-weekend-%40ytksato-e1517088415679-1200x588.jpg'
+yourImg.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+item16.innerText = "Won't get fooled again.";
